ee409d642c02ce6aa87d9f94e91df815a499fe7a
Gourab Panigrahi <pgourab@iisc.ac.in>
2025-04-08 13:38:34 +0530

    commit

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -6,28 +6,28 @@ export const site: SiteConfig = {
     bio: 'PhD Student <br>  <a href="https://sites.google.com/view/matrix-lab" target="_blank" rel="noopener noreferrer" style="color: #007acc;"> MATRIX Lab</a>  <br> <a href="https://cds.iisc.ac.in/" target="_blank" rel="noopener noreferrer" style="color: #007acc;"> Department of Computational Data Sciences</a> <br> <a href="https://iisc.ac.in" target="_blank" rel="noopener noreferrer" style="color: #007acc;">Indian Institute of Science (IISc), Bangalore</a> <br>',
     name: 'Gourab Panigrahi',
     // status: '🌸',
-    // metadata: [
-    //   {
-    //     icon: 'i-simple-icons-github',
-    //     link: 'https://github.com/gourabp7'
-    //   },
-    //   {
-    //     icon: 'i-simple-icons-googlescholar',
-    //     link: 'https://scholar.google.com/citations?user=Cn4q0OQAAAAJ&hl=en'
-    //   },
-    //   {
-    //     icon: 'i-simple-icons-linkedin',
-    //     link: 'https://www.linkedin.com/in/gourab-panigrahi-083939237'
-    //   },
-    //   {
-    //     icon: 'i-simple-icons-orcid',
-    //     link: 'https://orcid.org/0000-0001-8121-3320'
-    //   },
-    //   {
-    //     icon: 'i-simple-icons-acm',
-    //     link: 'https://dl.acm.org/profile/99661043822'
-    //   }
-    // ]
+    metadata: [
+      {
+        icon: 'i-simple-icons-github',
+        link: 'https://github.com/gourabp7'
+      },
+      {
+        icon: 'i-simple-icons-googlescholar',
+        link: 'https://scholar.google.com/citations?user=Cn4q0OQAAAAJ&hl=en'
+      },
+      {
+        icon: 'i-simple-icons-linkedin',
+        link: 'https://www.linkedin.com/in/gourab-panigrahi-083939237'
+      },
+      {
+        icon: 'i-simple-icons-orcid',
+        link: 'https://orcid.org/0000-0001-8121-3320'
+      },
+      {
+        icon: 'i-simple-icons-acm',
+        link: 'https://dl.acm.org/profile/99661043822'
+      }
+    ]
   },
   description: 'Powered by SvelteKit/Urara',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'https://gourabp7.github.io/',
@@ -37,7 +37,3 @@ export const site: SiteConfig = {
   themeColor: '#3D4451',
   title: 'Gourab Panigrahi',
 }
-
-// <a href="https://linkedin.com/in/your-linkedin-profile" target="_blank" rel="noopener noreferrer"> <img src="/assets/linkedin_icon.png" alt="LinkedIn"> LinkedIn </a>
-
-// style="font-size: 24px; vertical-align: middle; margin-right: 8px;"
